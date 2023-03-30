f58233ba365bcc00cbb4efe8f062e0c653e60a9f
Amandine Latour <natsukichan13@hotmail.fr>
2023-03-30 16:03:41 +0200

    create a auth system with nextjs and supabase

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,28 @@
+import { Auth } from "@supabase/auth-ui-react";
 import HeaderLoginMobile from "./components/headerLoginMobile";
+import { supabase } from "./../lib/supabaseClient";
+
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function Login() {
-  return <HeaderLoginMobile />;
+  async function signInWithEmail() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: "example@email.com",
+      password: "example-password",
+    });
+
+    async function signOut() {
+      const { error } = await supabase.auth.signOut();
+    }
+
+    return (
+      <>
+        <HeaderLoginMobile />
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+        ></Auth>
+      </>
+    );
+  }
 }
